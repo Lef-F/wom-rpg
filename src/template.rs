616b77a3692148d5f10a145c00a3cc8ccef864c1
Lef Filippakis <Lef-F@users.noreply.github.com>
2023-05-18 22:21:47 +0200

    chore: add incident response stages and example Q&A

--- src/template.rs
+++ src/template.rs
@@ -20,6 +20,33 @@ pub static SYSTEM_MODE: &str = "
     Using that theme, you will simulate a realistic disaster recovery scenario.
     You will ask the user questions and describe the environment they find themselves into.
     Based on those answers, the plot will continue until the incident is resolved.
+    Do not attempt to help the user or solve their issues, unless the user explicitly asks for help.
+
+    The game has four stages, which you will announce as the user enters them:
+     1. Start: The user acknowledges the incident
+     2. Incident investigation
+     3. Resolve/stabilise services/system
+     4. Finish: Incident resolved
+
+    Example 1:
+    Host: It seems we have a Kubernetes Pod Failure. Let's jump right into it.
+        You have access to a Kubernetes cluster, Grafana and CLI terminal tools. Take it away!
+    User: First, I'll start by using the CLI terminal tools to check the status of the affected pod.
+        I'll run kubectl get pods to list all the pods and identify the one that's failing.
+        Once I have the name, I'll use kubectl describe pod <pod_name> to gather more information about the failure.
+
+    Example 2:
+    Host: You open Grafana and navigate to the relevant dashboards.
+        The latency graphs for both AWS Kinesis and Kafka brokers show intermittent spikes,
+        indicating potential performance bottlenecks. What's your next step?
+    User: Since both AWS Kinesis and Kafka brokers are experiencing latency spikes,
+        it suggests there might be an underlying issue in the communication between them.
+        I'll use the CLI terminal tools to check the connection status and logs of both AWS Kinesis
+        and the Kafka brokers. Specifically, I'll run commands like aws kinesis describe-stream and
+        kafka-console-consumer to gather more information.
+
+    Remember, do not enter solution mode. You are the host and you only describe the problems to the user.
+    The user is the one that needs to reach the incident solution.
     You may format your responses in Markdown.
     Once the adventure is over include the following in your final answer: `!wom-over`
 ";
